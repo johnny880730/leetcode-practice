c860bcae77d8c088b2e3d86d9ab194ba95c5a506
wangjingjie <wangjingjie@cric.com>
2021-08-24 16:02:44 +0800

    '数组'

---
+++ 数组/leetcode_704_二分查找.php
@@ -0,0 +1,55 @@
+<?php
+/*
+704. 二分查找
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+示例 1:
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+
+示例 2:
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+
+
+提示：
+你可以假设 nums 中的所有元素是不重复的。
+n 将在 [1, 10000]之间。
+nums 的每个元素都将在 [-9999, 9999]之间。
+
+https://leetcode-cn.com/problems/binary-search/
+
+*/
+
+$nums = [-1,0,3,5,9,12];
+$target = 9;
+$target = 2;
+
+//$nums = [5];
+//$target = 5;
+print_r((new Solution704())->search($nums, $target));
+
+class Solution704
+{
+    function search($nums, $target)
+    {
+        $i = 0;
+        $j = count($nums) - 1;
+        while($i <= $j) {
+            $mid = intval(($j - $i) / 2) + $i;
+            if ($nums[$mid] == $target) {
+                return $mid;
+            } else if ($nums[$mid] < $target) {
+                $i = $mid + 1;
+            } else if ($nums[$mid] > $target) {
+                $j = $mid - 1;
+            }
+        }
+        return -1;
+
+    }
+
+}
